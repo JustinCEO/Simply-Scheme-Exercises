3b68c6b617da5875e24c47bafa6ad355007a0508
JustinCEO <justinceo@gmail.com>
2020-10-16 17:04:57 -0400

    Some chapter 1 examples

---
+++ Chapter 1/acronym_example.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(require "definitions/simply_redef.scm")
+
+
+(define (acronym phrase)
+  (accumulate word (every first (keep real-word? phrase))))
+
+(define (real-word? wd)
+  (not (member? wd '(a the an in of and for to with))))
